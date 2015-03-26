883f3810f334a7b3234af9b979b2089b5ba09429
Jules <julesfpollender@gmail.com>
2015-03-26 11:15:34 -0400

    transition et main

---
+++ tp2/sources/Main.cpp
@@ -0,0 +1,20 @@
+/****************************************************************************
+* INF2810 - TP2 - Hiver 2015
+* Fichier       : main.cpp
+* Auteur        : Jules Favreau-Pollender, Francis Rochon, Samuel Rondeau
+* Date          : 26 mars 2015
+* Mise à jour   : -- avril 2015
+* Description   : Modélisation d'automates
+****************************************************************************/
+
+#include "headers/Automate.h"
+#include "headers/Etat.h"
+#include "headers/Transition.h"
+
+using namespace std;
+
+int main()
+{
+
+	system("PAUSE");
+}
---
+++ tp2/sources/Transition.cpp
@@ -0,0 +1,53 @@
+/****************************************************************************
+* Fichier       : Transition.cpp
+* Auteur        : Jules Favreau-Pollender, Francis Rochon, Samuel Rondeau
+* Date          : 26 mars 2015
+* Mise à jour   : 09 mars 2015
+* Description   : Implementation de la classe Transition
+****************************************************************************/
+
+#include "headers/Transition.h"
+
+/****************************************************************************
+* Fonction		: Transition::Transition
+* Description	: Constructeur par defaut
+* Paramètres	: aucun
+* Retour		: aucun
+****************************************************************************/
+Transition::Transition()
+{
+	_type = MOORE;
+	_entre = "";
+	_sortie = "";
+	_etiquette = "";
+}
+
+/****************************************************************************
+* Fonction		: Transition::Transition
+* Description	: Constructeur par paramètre
+* Paramètres	: (int) type : le type de la machine
+				  (string) entre : le input de la transition
+				  (string) sortie : la sortie de la transition initilament vide si machine de moore
+* Retour		: aucun
+****************************************************************************/
+Transition::Transition(Type type, string entre, string sortie = "")
+{
+	_type = type;
+	_entre = entre;
+	_sortie = sortie;
+
+	_etiquette = (type == MEALY) ? _entre + ", " + _sortie : _entre;
+
+
+}
+
+/****************************************************************************
+* Fonction		: Transition::~Transition
+* Description	: Destructeur
+* Paramètres	: aucun
+* Retour		: aucun
+****************************************************************************/
+Transition::~Transition()
+{
+
+}
